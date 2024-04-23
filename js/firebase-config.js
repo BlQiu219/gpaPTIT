2e629da70bf2304d2b958d81d563766f63438c8a
Blqiu <nguyenththu219@gmail.com>
2024-04-23 10:03:24 +0700

    Add files via upload

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAFCtjhp0oPAcBjAUhX17IMk95hoCHT9oc",
-    authDomain: "gpaptit.firebaseapp.com",
-    projectId: "gpaptit",
-    storageBucket: "gpaptit.appspot.com",
-    messagingSenderId: "63644930373",
-    appId: "1:63644930373:web:89b40396efca0db11aae2c",
-    measurementId: "G-4TNG23FZ45"
-  };
